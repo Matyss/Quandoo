a3446cf9b0597b0fb9d7eac282cdf9a4bf9ebb71
Mateusz <mateusz@Witajs-MacBook-Pro.local>
2018-04-30 14:16:57 +0200

    adjusted relative path, comment

--- src/main/java/com/quandoo/resources/Base.java
+++ src/main/java/com/quandoo/resources/Base.java
@@ -25,13 +25,14 @@ public class Base {
 
         //Configuration object
         Properties props = new Properties();
-        FileInputStream inputFile = new FileInputStream("/Users/mateusz/Documents/Testing/Testing/Quandoo.nosync/src/main/java/com/quandoo/resources/configFile.properties");
+        FileInputStream inputFile = new FileInputStream("src/main/java/com/quandoo/resources/configFile.properties");
 
         props.load(inputFile);
         String browserName = props
                     .getProperty("browser")
                     .toLowerCase();
 
+        //Set the drivers' local path
         System.setProperty("webdriver.chrome.driver", "/Users/mateusz/Documents/Testing/chromedriver");
 
         switch (browserName) {
